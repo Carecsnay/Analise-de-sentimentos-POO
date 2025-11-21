c67061a539c534f7034586192b63aeea6f27b2da
Bruno Escritório <carecsnay1@gmail.com>
2025-11-21 10:32:17 -0300

    chore: add db SQLITE3 configs

---
+++ db/database.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+class Database:
+    def __init__(self,db_name):
+        self.connect = sqlite3.connect(db_name)
+        self.cursor = self.connect.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS analysis (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            post TEXT NOT NULL,
+            category TEXT
+            )
+        """)
+        self.connect.commit()
+    
+    def insert_post(self, post, category):
+        self.cursor.execute("INSERT INTO analysis (post, categoria) VALUES (?,?)", post, category)
+        self.connect.commit()
+
+    def read_posts(self):
+        self.cursor.execute("SELECT * FROM analysis")
+        return self.cursor.fetchall()
+    
+    def __del__(self):
+        self.connect.close()
